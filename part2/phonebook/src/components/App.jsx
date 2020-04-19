2ef5ec0f6f20ced61b22715aba5bf11d80bbd48f
Renato Porto <renatoafporto@gmail.com>
2020-04-19 03:27:04 -0300

    Started implementation of phonebook

---
+++ part2/phonebook/src/components/App.jsx
@@ -0,0 +1,69 @@
+import React, { useState } from "react";
+
+const App = () => {
+  const [persons, setPersons] = useState([
+    { name: "Arto Hellas", number: "040-1234567" },
+    { name: "Ada Lovelace", number: "39-44-5323523" },
+    { name: "Dan Abramov", number: "12-43-234345" },
+    { name: "Mary Poppendieck", number: "39-23-6423122" },
+  ]);
+  const [newName, setNewName] = useState("");
+  const [newNumber, setNewNumber] = useState("");
+  const [showWith, setShowWith] = useState("");
+
+  const addName = (event) => {
+    event.preventDefault();
+    if (!persons.every((person) => person.name !== newName)) {
+      return alert(`${newName} is already added to phonebook`);
+    }
+    setPersons(persons.concat({ name: newName, number: newNumber }));
+    setNewName("");
+    setNewNumber("");
+  };
+
+  const handleNameChange = (event) => {
+    setNewName(event.target.value);
+  };
+
+  const handleNumberChange = (event) => {
+    setNewNumber(event.target.value);
+  };
+
+  const handleShowWithChange = (event) => {
+    setShowWith(event.target.value);
+  };
+
+  const personsToShow = persons.filter((person) =>
+    person.name.toLowerCase().includes(showWith.toLowerCase())
+  );
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <h2>Add new contact</h2>
+      <form onSubmit={addName}>
+        <div>
+          name: <input value={newName} onChange={handleNameChange} />
+        </div>
+        <div>
+          number: <input value={newNumber} onChange={handleNumberChange} />
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      {personsToShow.map((person) => (
+        <div key={person.name}>
+          {person.name} {person.number}
+        </div>
+      ))}
+      <p>
+        filter shown with
+        <input value={showWith} onChange={handleShowWithChange} />
+      </p>
+    </div>
+  );
+};
+
+export default App;
