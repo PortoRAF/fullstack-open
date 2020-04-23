20f60c9615f7ae8fe43b3124f66b1aa777a53656
Renato Porto <renatoafporto@gmail.com>
2020-04-23 03:11:12 -0300

    Implemented PUT route and corrected error handling

--- part3/notes-backend/index.js
+++ part3/notes-backend/index.js
@@ -1,12 +1,16 @@
 const express = require("express");
 const cors = require("cors");
+const morgan = require("morgan");
 const Note = require("./models/note");
 
 const app = express();
 
 app.use(cors());
-app.use(express.json());
 app.use(express.static("build"));
+app.use(express.json());
+app.use(
+  morgan(":method :url :status :res[content-length] - :response-time ms :body")
+);
 
 let notes = [
   {
@@ -39,18 +43,22 @@ app.get("/api/notes", (req, res) => {
   });
 });
 
-app.get("/api/notes/:id", (req, res) => {
+app.get("/api/notes/:id", (req, res, next) => {
   Note.findById(req.params.id)
-    .then((note) => res.json(note))
-    .catch((error) => res.status(404).end());
+    .then((note) => {
+      if (note) {
+        res.json(note);
+      } else {
+        res.status(404).end();
+      }
+    })
+    .catch((error) => next(error));
 });
 
 app.delete("/api/notes/:id", (req, res) => {
   Note.findByIdAndDelete(req.params.id)
-    .then((response) =>
-      res.status(204).send(`deleted ${response.deletedCount}`)
-    )
-    .catch((error) => res.status(404).end());
+    .then((result) => res.status(204).end())
+    .catch((error) => next(error));
 });
 
 app.post("/api/notes", (req, res) => {
@@ -81,8 +89,25 @@ app.put("/api/notes/:id", (req, res) => {
 
   Note.findByIdAndUpdate(req.params.id, note, { new: true })
     .then((updatedNote) => res.json(updatedNote))
-    .catch((error) => res.status(404).end());
+    .catch((error) => next(error));
 });
+
+const unknownEndpoint = (req, res) => {
+  res.status(404).send({ error: "unknown endpoint" });
+};
+
+const errorHandler = (error, req, res, next) => {
+  console.error(error.message);
+
+  if (error.name === "CastError") {
+    return res.status(400).send({ error: "bad formatted id" });
+  }
+
+  next(error);
+};
+
+app.use(unknownEndpoint);
+app.use(errorHandler);
 
 const PORT = process.env.PORT;
 app.listen(PORT, () => {
--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -7,12 +7,12 @@ const app = express();
 
 morgan.token("body", (req) => (req.body.name ? JSON.stringify(req.body) : " "));
 
-app.use(express.json());
 app.use(cors());
+app.use(express.static("build"));
+app.use(express.json());
 app.use(
   morgan(":method :url :status :res[content-length] - :response-time ms :body")
 );
-app.use(express.static("build"));
 
 const MAX_CONTACTS = 10;
 
@@ -45,10 +45,10 @@ app.get("/", (req, res) => {
   );
 });
 
-app.get("/info", (req, res) => {
+app.get("/info", (req, res, next) => {
   Person.estimatedDocumentCount((error, count) => {
     if (error) {
-      return res.status(400).end();
+      next(error);
     }
     const date = new Date();
     res.send(`<p>Phonebook has info for ${count} people</p><p>${date}</p>`);
@@ -61,19 +61,25 @@ app.get("/api/persons", (req, res) => {
   });
 });
 
-app.get("/api/persons/:id", (req, res) => {
+app.get("/api/persons/:id", (req, res, next) => {
   Person.findById(req.params.id)
-    .then((person) => res.send(person))
-    .catch(() => res.status(404).end());
+    .then((person) => {
+      if (person) {
+        res.json(person);
+      } else {
+        res.status(404).end();
+      }
+    })
+    .catch((error) => next(error));
 });
 
-app.delete("/api/persons/:id", (req, res) => {
+app.delete("/api/persons/:id", (req, res, next) => {
   Person.findByIdAndDelete(req.params.id)
-    .then(() => res.status(204).end())
-    .catch(() => res.status(404).end());
+    .then((result) => res.status(204).end())
+    .catch((error) => next(error));
 });
 
-app.post("/api/persons", async (req, res) => {
+app.post("/api/persons", (req, res, next) => {
   if (!req.body.name) {
     return res.status(400).send({
       error: "name missing",
@@ -86,23 +92,61 @@ app.post("/api/persons", async (req, res) => {
   }
   Person.find({ name: req.body.name }).then((persons) => {
     if (persons.length > 0) {
-      return res.status(400).send({ error: "name must be unique" });
+      res.status(400).send("name must be unique");
+    } else {
+      const person = new Person({
+        name: req.body.name,
+        number: req.body.number,
+      });
+      person.save().then((person) => res.send(person));
     }
-
-    const person = new Person({
-      name: req.body.name,
-      number: req.body.number,
-    });
-
-    person.save().then((person) => res.send(person));
   });
+});
+
+app.put("/api/persons/:id", (req, res, next) => {
+  if (!req.body.name) {
+    return res.status(400).send({
+      error: "name missing",
+    });
+  }
+  if (!req.body.number) {
+    return res.status(400).send({
+      error: "number missing",
+    });
+  }
+
+  const person = {
+    name: req.body.name,
+    number: req.body.number,
+  };
+
+  Person.findByIdAndUpdate(req.params.id, person, { new: true })
+    .then((updatedPerson) => {
+      if (updatedPerson) {
+        res.json(updatedPerson);
+      } else {
+        res.status(404).end();
+      }
+    })
+    .catch((error) => next(error));
 });
 
 const unknownEndpoint = (req, res) => {
   res.status(404).send({ error: "unknown endpoint" });
 };
 
+const errorHandler = (error, req, res, next) => {
+  console.error(error.message);
+
+  if (error.name === "CastError") {
+    res.status(400).send({ error: "bad formatted id" });
+  }
+
+  next(error);
+};
+
 app.use(unknownEndpoint);
+app.use(errorHandler);
 
 const PORT = process.env.PORT;
 app.listen(PORT, () => console.log(`Server running at port ${PORT}`));
