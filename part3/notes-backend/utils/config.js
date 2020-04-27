2d83dfeee6813b6885e4c4de4b7b1e5ee216382d
Renato Porto <renatoafporto@gmail.com>
2020-04-27 20:22:49 -0300

    Included NODE_ENV and tests for fetching from api

--- part3/notes-backend/utils/config.js
+++ part3/notes-backend/utils/config.js
@@ -3,6 +3,10 @@ require('dotenv').config()
 let PORT = process.env.PORT
 let MONGODB_SERVER = process.env.MONGODB_SERVER
 
+if (process.env.NODE_ENV === 'test') {
+  MONGODB_SERVER = process.env.TEST_MONGODB_SERVER
+}
+
 module.exports = {
   MONGODB_SERVER,
   PORT
